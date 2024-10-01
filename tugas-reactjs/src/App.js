bec14a6af8cf191e15b278a0ab1b56329fdd90ec
Wahyu Aristya <wahyuaristya08@gmail.com>
2024-10-01 22:06:58 +0800

    Tugas 6 - React: Introduction

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className='App'>
+        <div className="App-header">
+          <h1>Hi, Saya Wahyu Aristya</h1>
+        </div>
+        <div>
+          <p>Saya dari batch 60 berhasil menginstal React</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
