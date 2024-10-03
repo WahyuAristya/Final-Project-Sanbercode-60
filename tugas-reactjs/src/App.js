4ef4a763d17ed114ffee0cc47337f6db464f9266
Wahyu Aristya <wahyuaristya08@gmail.com>
2024-10-03 19:26:51 +0800

    Tugas 8 - React : Hooks, State, Lifecycle Hooks, Handling event & Conditional rendering

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Tugas6 from './tugas6/tugas6';
 import Tugas7 from './tugas7/tugas7';
+import Tugas8 from './tugas8/tugas8';
 
 function App() {
   return (
     <>
       <Tugas6 />
       <Tugas7 name="Wahyu Aristya" batch="60" email="wahyuaristya08@gmail.com"/>
+      <Tugas8 />
     </>
   )
 }
