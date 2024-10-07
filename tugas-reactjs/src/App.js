63f11d0d8178fb244da062895d0bf9baf1d7d292
Wahyu Aristya <wahyuaristya08@gmail.com>
2024-10-07 21:33:30 +0800

    Tugas 10 - React CRUD : Fetching data

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -3,14 +3,16 @@ import Tugas6 from './tugas6/tugas6';
 import Tugas7 from './tugas7/tugas7';
 import Tugas8 from './tugas8/tugas8';
 import Tugas9 from './tugas9/tugas9';
+import Tugas10 from './tugas10/tugas10';
 
 function App() {
   return (
     <>
-      <Tugas6 />
+      {/* <Tugas6 />
       <Tugas7 name="Wahyu Aristya" batch="60" email="wahyuaristya08@gmail.com"/>
       <Tugas8 />
-      <Tugas9 />
+      <Tugas9 /> */}
+      <Tugas10 />
     </>
   )
 }
