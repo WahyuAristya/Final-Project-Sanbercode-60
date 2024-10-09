b5f0bb994dc4553adb346dc643961a5d4973cfda
Wahyu Aristya <wahyuaristya08@gmail.com>
2024-10-09 22:15:05 +0800

    Tugas 11 - React CRUD : Create & Delete data

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -4,6 +4,7 @@ import Tugas7 from './tugas7/tugas7';
 import Tugas8 from './tugas8/tugas8';
 import Tugas9 from './tugas9/tugas9';
 import Tugas10 from './tugas10/tugas10';
+import Tugas11 from './tugas11/tugas11';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Tugas7 name="Wahyu Aristya" batch="60" email="wahyuaristya08@gmail.com"/>
       <Tugas8 />
       <Tugas9 /> */}
-      <Tugas10 />
+      {/* <Tugas10 /> */}
+      <Tugas11 />
     </>
   )
 }
